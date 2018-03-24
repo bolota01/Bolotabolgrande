72fca774792353a527dbd62c9649ba8ec04b8fc1
Guizao01 <37387447+Guizao01@users.noreply.github.com>
2018-03-24 17:23:20 -0300

    2 carros novos balanceado o emprego
    adicionado 2 carros e diminuido o preço de compra dos cartoes

---
+++ resources/chevette/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+
+files {
+  'data/handling.meta',
+}
+
+client_script 'vehicle_names.lua'
